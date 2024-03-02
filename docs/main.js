76d7771ab62b624cff57f4576303e2df2c32bf67
msanch023 <mas023@ucsd.edu>
2024-03-01 18:40:34 -0800

    testing

--- docs/main.js
+++ docs/main.js
@@ -1,35 +1,5 @@
-d3.csv("top10.csv").then(function(data) {
 
-    // X axis: scale and draw:
-    const x = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.pts)])
-        .range([0, width]);
-    svg.append("g")
-        .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-          .attr("transform", "translate(-10,0)rotate(-45)")
-          .style("text-anchor", "end");
   
-    // Y axis: scale and draw:
-    const y = d3.scaleBand()
-        .range([0, height])
-        .domain(data.map(d => d.player))
-        .padding(.1);
-    svg.append("g")
-      .call(d3.axisLeft(y))
-  
-    //Bars
-    svg.selectAll("myRect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("x", x(0) )
-      .attr("y", d => y(d.player))
-      .attr("width", d => x(d.pts))
-      .attr("height", y.bandwidth())
-      .attr("class", "bar")
-  });
   
  //Button
   document.getElementById('sort').addEventListener('click', () => {
